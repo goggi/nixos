75069be81029b5738253497fe943060c320926f5
goggi <goran.cinklovic@gmail.com>
2023-06-17 22:57:55 +0200

    test

--- home/features/desktop/common/wayland/eww/default.nix
+++ home/features/desktop/common/wayland/eww/default.nix
@@ -1,55 +1,44 @@
 {
   inputs,
-  lib,
-  config,
   pkgs,
+  config,
   ...
-}:
-with lib; let
-  cfg = config.modules.eww;
-in {
-  options.modules.eww = {enable = mkEnableOption "eww";};
+}: {
+  # eww package
+  home.packages = with pkgs; [
+    eww-wayland
+    pamixer
+    brightnessctl
+    (nerdfonts.override {fonts = ["JetBrainsMono"];})
+  ];
 
-  config = mkIf cfg.enable {
-    # theres no programs.eww.enable here because eww looks for files in .config
-    # thats why we have all the home.files
+  # configuration
+  home.file.".config/eww/eww.scss".source = ./eww.scss;
+  home.file.".config/eww/eww.yuck".source = ./eww.yuck;
 
-    # eww package
-    home.packages = with pkgs; [
-      eww-wayland
-      pamixer
-      brightnessctl
-      (nerdfonts.override {fonts = ["JetBrainsMono"];})
-    ];
+  # scripts
+  home.file.".config/eww/scripts/battery.sh" = {
+    source = ./scripts/battery.sh;
+    executable = true;
+  };
 
-    # configuration
-    home.file.".config/eww/eww.scss".source = ./eww.scss;
-    home.file.".config/eww/eww.yuck".source = ./eww.yuck;
+  home.file.".config/eww/scripts/wifi.sh" = {
+    source = ./scripts/wifi.sh;
+    executable = true;
+  };
 
-    # scripts
-    home.file.".config/eww/scripts/battery.sh" = {
-      source = ./scripts/battery.sh;
-      executable = true;
-    };
+  home.file.".config/eww/scripts/brightness.sh" = {
+    source = ./scripts/brightness.sh;
+    executable = true;
+  };
 
-    home.file.".config/eww/scripts/wifi.sh" = {
-      source = ./scripts/wifi.sh;
-      executable = true;
-    };
+  home.file.".config/eww/scripts/workspaces.sh" = {
+    source = ./scripts/workspaces.sh;
+    executable = true;
+  };
 
-    home.file.".config/eww/scripts/brightness.sh" = {
-      source = ./scripts/brightness.sh;
-      executable = true;
-    };
-
-    home.file.".config/eww/scripts/workspaces.sh" = {
-      source = ./scripts/workspaces.sh;
-      executable = true;
-    };
-
-    home.file.".config/eww/scripts/workspaces.lua" = {
-      source = ./scripts/workspaces.lua;
-      executable = true;
-    };
+  home.file.".config/eww/scripts/workspaces.lua" = {
+    source = ./scripts/workspaces.lua;
+    executable = true;
   };
 }
